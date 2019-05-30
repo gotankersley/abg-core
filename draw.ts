1354a50f3cddcf9feb8c48efa65b0ff6b2318127
gotankersley <gotankersley@harding.edu>
2019-05-30 16:24:25 -0500

    drawlib

---
+++ draw.ts
@@ -0,0 +1,16 @@
+export function drawLine(ctx:CanvasRenderingContext2D, x1:number, y1:number, x2:number, y2:number) {
+	ctx.beginPath();	
+	ctx.moveTo(x1, y1);
+	
+	ctx.lineTo(x2,y2);    
+    ctx.closePath();		
+	ctx.stroke();   
+}
+
+export function drawCircle(ctx:CanvasRenderingContext2D, x:number, y:number, r:number) {
+	ctx.beginPath();    
+	ctx.arc(x + r,y + r, r, 0, 2 * Math.PI, true);	
+	ctx.closePath();    
+	ctx.fill();		
+}
+
